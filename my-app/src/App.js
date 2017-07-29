09edbb7656e79c749701f3b88a4638027bec71e3
reecefenwick <fenwickreece08@gmail.com>
2017-07-29 16:10:34 +1000

    Add react-bootstrap and typeahead

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Typeahead } from 'react-bootstrap-typeahead';
 import './App.css';
+import EmploymentProjections from './data/EmploymentProjections'
 
 class App extends Component {
   render() {
+    console.log(EmploymentProjections);
+    const options = [
+      'John',
+      'Miles',
+      'Charles',
+      'Herbie',
+    ];
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +22,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Typeahead
+          labelKey="name"
+          options={options}
+          placeholder="Choose a state..."
+        />
       </div>
     );
   }
